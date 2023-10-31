91876905b49777f01651b34c7602db548c4161ae
Nekic Lovro <lovneki@pr4-09.studenti.math.hr>
2023-10-31 15:57:38 +0100

    Zad1

--- src/min-max.cc
+++ src/min-max.cc
@@ -1,5 +1,5 @@
 #ifdef HAVE_CONFIG_H
-# include "config.h"     
+# include "config.h"
 #endif
 #include <iostream>
 #include <vector>
@@ -8,49 +8,91 @@
 #include <limits>
 
 #include "dune/common/parallel/mpihelper.hh"
-#include <dune/common/exceptions.hh> 
+#include <dune/common/exceptions.hh>
 
-#include <dune/grid/uggrid.hh>  
-#include <dune/grid/common/gridinfo.hh> 
+#include <dune/grid/uggrid.hh>
+#include <dune/grid/common/gridinfo.hh>
 #include <dune/grid/io/file/gmshreader.hh>
+#include <dune/common/fvector.hh>
 
 #include <dune/grid/io/file/vtk/vtkwriter.hh>
 
-// Izračunaj kut između p1-p2 i p3-p2
+
 template <typename Point>
-double kut(Point const & p1, Point const & p2, Point const & p3)
-{
-   // računanje kuta (VAŠ KOD) ide ovdje    
-   // Point će biti Dune::FieldVector<double, dim>. Norma se računa 
-   // pomoću funkcije članice klase two_norm(), a skalarni produkt 
-   // pomoću funkcije članice klase dot(). Pogledati dokumentaciju klase
-   //  Dune::FieldVector<double, dim>.
+double kut(Point const & p1, Point const & p2, Point const & p3){
+    Dune::FieldVector<double, 2> vector1 = p1 - p2;
+    Dune::FieldVector<double, 2> vector2 = p3 - p2;
+    double dotProd = dot(vector1,vector2);
+    double normProd = vector1.two_norm() * vector2.two_norm();
+    double kut = std::acos(dotProd / normProd);
+    kut = 180 * kut / M_PI;
     return kut;
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
     const int dim = 2;
     using GridType = Dune::UGGrid<dim>;
-    using LeafGridView = ...;
-     
-    // UČITATI 2D MREŽU IZ GMSH DATOTEKE KOJA JE ZADANA KAO ARGUMENT KOMANDNE LINIJE.
-    
-    // gridView JE LeafGridView!
-    for(auto const & element : elements(gridView))
-    {
+    using LeafGridView = GridType::LeafGridView;
 
-/*     VAŠ KOD dolazi ovdje.
- *     RAČUNATI MIN I MAX KUT U SVAKOM ELEMENTU. 
-*/
-    } 
+    Dune::MPIHelper::instance(argc, argv);
 
-   // ISPISATI BROJ ELEMENATA; MINIMALNI I MAKSIMALNI KUT U STUPNJEVIMA:
-    
+    if(argc < 3){
+       std::cerr << "Usage: " << argv[0]
+                 << " ime_grid_datoteke.msh br_profinjenja" << std::endl;
+       std::exit(1);
+    }
+    double min_kut = 1000;
+    double max_kut = -1000;
+    int min_ind = 0;
+    int max_ind = 0;
+    int i = 0;
+    double kut1;
 
-    // Ispis mreže u VTK formatu (u datoteku poluvijenac.vtu)
-    Dune::VTKWriter<LeafGridView> vtkwriter(gridView);
+    bool verbosity = true;
+    bool insertBoundarySegments = false;
+
+    std::unique_ptr<GridType> pgrid = Dune::GmshReader<GridType>::read(argv[1],
+                                                        verbosity, insertBoundarySegments);
+
+    int no_r = std::stoi(argv[2]);
+    pgrid->globalRefine(no_r);
+    auto gv = pgrid->leafGridView();
+    for(auto const & element : elements(gv)){
+        kut1 = kut(element.geometry().corner(0),element.geometry().corner(1), element.geometry().corner(2));
+        if(kut1 < min_kut){
+            min_kut = kut1;
+            min_ind = i;
+        }
+        if(kut1 > max_kut){
+            max_kut = kut1;
+            max_ind = i;
+        }
+
+        kut1 = kut(element.geometry().corner(1),element.geometry().corner(2), element.geometry().corner(0));
+        if(kut1 < min_kut){
+            min_kut = kut1;
+            min_ind = i;
+        }
+        if(kut1 > max_kut){
+            max_kut = kut1;
+            max_ind = i;
+        }
+
+        kut1 = kut(element.geometry().corner(2),element.geometry().corner(0), element.geometry().corner(1));
+        if(kut1 < min_kut){
+            min_kut = kut1;
+            min_ind = i;
+        }
+        if(kut1 > max_kut){
+            max_kut = kut1;
+            max_ind = i;
+        }
+        i++;
+    }
+    std::cout << "Broj elemenata: " << i << "\n";
+    std::cout << "Minimalni kut je " << min_kut << " i postiže se na indeksu " << min_ind << "\n";
+    std::cout << "Maksimalni kut je " << max_kut << " i postiže se na indeksu " << max_ind << "\n";
+    Dune::VTKWriter<LeafGridView> vtkwriter(gv);
     vtkwriter.write("poluvijenac");
-
     return 0;
 }
